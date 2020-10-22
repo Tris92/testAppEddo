4e87fcd8f0b0fe51d0dbbfc782701e3abc9d251a
Eddo <eddo@macbookpro-tristan.local>
2020-10-22 15:32:12 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Platform, Image, Text, View } from "react-native";
+import {
+  createSwitchNavigator,
+  createStackNavigator,
+  createAppContainer
+} from "react-navigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import * as firebase from "firebase";
+var firebaseConfig = {
+  apiKey: "AIzaSyBNw9f2cVDdb3uB00TSOupaS8iI1zygw1U",
+  authDomain: "apptest-af6ba.firebaseapp.com",
+  databaseURL: "https://apptest-af6ba.firebaseio.com",
+  projectId: "apptest-af6ba",
+  storageBucket: "apptest-af6ba.appspot.com",
+  messagingSenderId: "130288428657",
+  appId: "1:130288428657:web:4b271c25d7af5ed0cd9dd0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+// import the different screens
+import Loading from "./screens/Loading";
+import SignUp from "./screens/SignUp";
+import Login from "./screens/Login";
+import Main from "./screens/Main";
+// import Otp from "./components/Phone/Otp";
+
+// create our app's navigation stack
+const RootStack = createSwitchNavigator(
+  {
+    Loading: Loading,
+    SignUp: SignUp,
+    Login: Login,
+    Main: Main,
+    // Otp: Otp
   },
-});
+  {
+    initialRouteName: "Loading"
+  }
+);
+
+const App = createAppContainer(RootStack);
+export default App;
